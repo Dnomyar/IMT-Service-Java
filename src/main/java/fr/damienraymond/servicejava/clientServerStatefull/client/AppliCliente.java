6c4e34ab588d85ce258c0bea694564b5d8e3fab8
Damien RAYMOND <dam.ray49@gmail.com>
2017-04-10 11:19:42 +0200

    Fix address of client server statefull

--- src/main/java/fr/damienraymond/servicejava/clientServerStatefull/client/AppliCliente.java
+++ src/main/java/fr/damienraymond/servicejava/clientServerStatefull/client/AppliCliente.java
@@ -27,7 +27,7 @@ public class AppliCliente {
 
     public static void main(String[] args) {
 
-        String adresse = "http://localhost:8080/servicejava/clientServerStatefull";
+        String adresse = "http://localhost:8080/servicejava/serverStatefull";
 
         System.out.println("*************");
 
