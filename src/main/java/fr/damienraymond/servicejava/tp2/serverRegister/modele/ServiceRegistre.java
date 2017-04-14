fe5b4c76e294b094e2a6eb020b1d54233b6faffe
Damien RAYMOND <dam.ray49@gmail.com>
2017-04-14 08:50:00 +0200

    Add annotations to ServiceRegistre iface

--- src/main/java/fr/damienraymond/servicejava/tp2/serverRegister/modele/ServiceRegistre.java
+++ src/main/java/fr/damienraymond/servicejava/tp2/serverRegister/modele/ServiceRegistre.java
@@ -1,9 +1,20 @@
 package fr.damienraymond.servicejava.tp2.serverRegister.modele;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 public interface ServiceRegistre {
 
+    @PUT
+    @Consumes(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_XML)
     Ressource set(Ressource n);
 
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
     Ressource get();
 }
 
