60b00f5a8815eae17aa74b2b650fd0c6e88d4f3c
Damien RAYMOND <dam.ray49@gmail.com>
2017-04-14 11:54:36 +0200

    Add filters to API

--- src/main/java/fr/damienraymond/servicejava/tp2/serverRegister/modele/ServiceRegistre.java
+++ src/main/java/fr/damienraymond/servicejava/tp2/serverRegister/modele/ServiceRegistre.java
@@ -1,5 +1,7 @@
 package fr.damienraymond.servicejava.tp2.serverRegister.modele;
 
+import fr.damienraymond.servicejava.tp2.serverRegister.infrastructure.jaxrs.annotations.*;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
@@ -11,10 +13,22 @@ public interface ServiceRegistre {
     @PUT
     @Consumes(MediaType.APPLICATION_XML)
     @Produces(MediaType.APPLICATION_XML)
+    @StatRequetes
+    @StatReponses
+    @AtomiciteRequeteReponseServeur
+    @CacheClient
+    @EcritureOptimiste
+    @VersionReponses
     Ressource set(Ressource n);
 
     @GET
     @Produces(MediaType.APPLICATION_XML)
+    @StatRequetes
+    @StatReponses
+    @AtomiciteRequeteReponseServeur
+    @CacheClient
+    @EcritureOptimiste
+    @VersionReponses
     Ressource get();
 }
 
