cf7ad1f06f17bae3603c87a8c31ee4714d095f38
Damien RAYMOND <dam.ray49@gmail.com>
2017-04-14 11:57:47 +0200

    Refactor AjouterPrecondition

--- src/main/java/fr/damienraymond/servicejava/tp2/clientRegister/infrastructure/jaxrs/AjouterPrecondition.java
+++ src/main/java/fr/damienraymond/servicejava/tp2/clientRegister/infrastructure/jaxrs/AjouterPrecondition.java
@@ -24,27 +24,22 @@ public class AjouterPrecondition implements ClientRequestFilter {
         System.out.println("- Cache partagé : " + this.cache);
     }
 
-    @Override
     /*
 	 * Filtre / requête.
 	 */
+    @Override
     public synchronized void filter(ClientRequestContext requete)
             throws IOException {
 
         // Premier cas : requête PUT
-        if (requete.getMethod().equalsIgnoreCase("PUT")) {
-            // Si la version est définie dans le cache, l'affecter au champ
-            // HttpHeaders.IF_MATCH
-            // de l'en-tête de la requête.
-            // Cf. ClientRequestContext.getHeaders().
-            if (cache.version != null) {
-                MultivaluedMap<String, Object> enTetes = requete.getHeaders();
-                enTetes.putSingle(HttpHeaders.IF_MATCH,
-                        cache.version);
-            }
+        if (handlePutRequest(requete)) {
             return;
         }
         // Second cas : requête GET
+        handleGetRequest(requete);
+    }
+
+    private void handleGetRequest(ClientRequestContext requete) {
         if (requete.getMethod().equalsIgnoreCase("GET")) {
             // Si la version est définie dans le cache, l'affecter au champ
             // HttpHeaders.IF_NONE_MATCH
@@ -57,6 +52,22 @@ public class AjouterPrecondition implements ClientRequestFilter {
             }
             return;
         }
+    }
+
+    private boolean handlePutRequest(ClientRequestContext requete) {
+        if (requete.getMethod().equalsIgnoreCase("PUT")) {
+            // Si la version est définie dans le cache, l'affecter au champ
+            // HttpHeaders.IF_MATCH
+            // de l'en-tête de la requête.
+            // Cf. ClientRequestContext.getHeaders().
+            if (cache.version != null) {
+                MultivaluedMap<String, Object> enTetes = requete.getHeaders();
+                enTetes.putSingle(HttpHeaders.IF_MATCH,
+                        cache.version);
+            }
+            return true;
+        }
+        return false;
     }
 
 }
