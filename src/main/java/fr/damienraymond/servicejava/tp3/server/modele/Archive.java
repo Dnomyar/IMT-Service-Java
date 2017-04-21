c63cf1f6916f1bbb5c3904a57d26ae4a69dd30e5
Damien RAYMOND <dam.ray49@gmail.com>
2017-04-21 09:51:00 +0200

    Add annotation to match web standards

--- src/main/java/fr/damienraymond/servicejava/tp3/server/modele/Archive.java
+++ src/main/java/fr/damienraymond/servicejava/tp3/server/modele/Archive.java
@@ -1,5 +1,7 @@
 package fr.damienraymond.servicejava.tp3.server.modele;
 
+import fr.damienraymond.servicejava.tp3.server.infrastructure.jaxrs.annotations.ReponsesNull404GET;
+
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
@@ -7,6 +9,7 @@ public interface Archive {
 
     // Here, no HTTP method annotation because the process is delegated to the sub resource
     @Path("{id}")
+    @ReponsesNull404GET
 	LivreRessource sousRessource(@PathParam("id") IdentifiantLivre id); // Une sous-ressource ne peut-être une cible de requête.
 
 }
