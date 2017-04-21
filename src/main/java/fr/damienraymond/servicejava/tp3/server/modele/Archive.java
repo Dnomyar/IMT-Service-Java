a167842e2a6fa8de60deabd19e477bb0fa23d048
Damien RAYMOND <dam.ray49@gmail.com>
2017-04-21 09:46:12 +0200

    Add comment to explain why there is no HTTP method annotation

--- src/main/java/fr/damienraymond/servicejava/tp3/server/modele/Archive.java
+++ src/main/java/fr/damienraymond/servicejava/tp3/server/modele/Archive.java
@@ -4,6 +4,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
 public interface Archive {
+
+    // Here, no HTTP method annotation because the process is delegated to the sub resource
     @Path("{id}")
 	LivreRessource sousRessource(@PathParam("id") IdentifiantLivre id); // Une sous-ressource ne peut-être une cible de requête.
 
