e908b345239810c7ceb903bdb63f425d673a6bed
Damien RAYMOND <dam.ray49@gmail.com>
2017-04-21 10:04:40 +0200

    Add server annotations to client

--- src/main/java/fr/damienraymond/servicejava/tp3/client/modele/Archive.java
+++ src/main/java/fr/damienraymond/servicejava/tp3/client/modele/Archive.java
@@ -1,6 +1,13 @@
 package fr.damienraymond.servicejava.tp3.client.modele;
 
-public interface Archive {
-    LivreRessource sousRessource(IdentifiantLivre id); // Une sous-ressource ne peut-être une cible de requête.
+import fr.damienraymond.servicejava.tp3.server.infrastructure.jaxrs.annotations.ReponsesNull404GET;
 
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+public interface Archive {
+    // Here, no HTTP method annotation because the process is delegated to the sub resource
+    @Path("{id}")
+    @ReponsesNull404GET
+    LivreRessource sousRessource(@PathParam("id") IdentifiantLivre id); // Une sous-ressource ne peut-être une cible de requête.
 }
--- src/main/java/fr/damienraymond/servicejava/tp3/client/modele/Bibliotheque.java
+++ src/main/java/fr/damienraymond/servicejava/tp3/client/modele/Bibliotheque.java
@@ -1,13 +1,31 @@
 package fr.damienraymond.servicejava.tp3.client.modele;
 
+import fr.damienraymond.servicejava.tp3.client.configuration.JAXRS;
 import fr.damienraymond.servicejava.tp3.client.infrastructure.jaxrs.HyperLien;
 import fr.damienraymond.servicejava.tp3.client.infrastructure.jaxrs.HyperLiens;
+import fr.damienraymond.servicejava.tp3.client.infrastructure.jaxrs.annotations.ReponsesCreatedPOST;
+import fr.damienraymond.servicejava.tp3.server.infrastructure.jaxrs.annotations.ReponsesNull404GET;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 
 public interface Bibliotheque {
+
+    @POST
+    @ReponsesCreatedPOST
+    @Consumes(JAXRS.TYPE_MEDIA)
+    @Produces(JAXRS.TYPE_MEDIA)
     HyperLien<LivreRessource> ajouter(Livre l);
 
-    HyperLien<LivreRessource> chercher(Livre l);
+    @GET
+    @ReponsesNull404GET
+    @Produces(JAXRS.TYPE_MEDIA)
+    HyperLien<LivreRessource> chercher(@QueryParam(value = JAXRS.CLE_TITRE) Livre l);
 
+    @GET
+    @Path(JAXRS.SOUSCHEMIN_CATALOGUE)
+    @Produces(MediaType.APPLICATION_XML)
     HyperLiens<LivreRessource> repertorier();
+
 
 }
