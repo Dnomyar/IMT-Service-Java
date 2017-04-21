f66457423b98d757e7e0672aa4e8c8f0ab791671
Damien RAYMOND <dam.ray49@gmail.com>
2017-04-21 09:44:34 +0200

    Add annotations to match web standards

--- src/main/java/fr/damienraymond/servicejava/tp3/server/modele/Bibliotheque.java
+++ src/main/java/fr/damienraymond/servicejava/tp3/server/modele/Bibliotheque.java
@@ -3,6 +3,8 @@ package fr.damienraymond.servicejava.tp3.server.modele;
 import fr.damienraymond.servicejava.tp3.server.configuration.JAXRS;
 import fr.damienraymond.servicejava.tp3.server.infrastructure.jaxrs.HyperLien;
 import fr.damienraymond.servicejava.tp3.server.infrastructure.jaxrs.HyperLiens;
+import fr.damienraymond.servicejava.tp3.server.infrastructure.jaxrs.annotations.ReponsesCreatedPOST;
+import fr.damienraymond.servicejava.tp3.server.infrastructure.jaxrs.annotations.ReponsesNull404GET;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -11,11 +13,13 @@ import java.awt.*;
 public interface Bibliotheque {
 
     @POST
+    @ReponsesCreatedPOST
     @Consumes(JAXRS.TYPE_MEDIA)
     @Produces(JAXRS.TYPE_MEDIA)
     HyperLien<LivreRessource> ajouter(Livre l);
 
     @GET
+    @ReponsesNull404GET
     @Produces(JAXRS.TYPE_MEDIA)
     HyperLien<LivreRessource> chercher(@QueryParam(value = JAXRS.CLE_TITRE) Livre l);
 
